ff41b99ac3349217331a0e371893f035fd935217
Wataru Haniyama <haniyama.wataru+job@gmail.com>
2022-01-28 12:42:32 +0900

    add action test

---
+++ .github/actions/jpcssblogdev-token/index.js
@@ -0,0 +1,43 @@
+const core = require('@actions/core');
+const axios = require('axios');
+
+const ENDPOINRT = "https://jpcssblogdev.azurewebsites.net/api";
+const TOKEN_ENDPOINT = `${ENDPOINRT}/auth/token`
+
+async function run() {
+
+  try {
+    const ACTIONS_ID_TOKEN_REQUEST_URL = process.env.ACTIONS_ID_TOKEN_REQUEST_URL;
+    const ACTIONS_ID_TOKEN_REQUEST_TOKEN = process.env.ACTIONS_ID_TOKEN_REQUEST_TOKEN;
+    const AUDIENCE = "api://jpcssblogdev.azurewebsites.net"
+    
+    const githubTokenRes = await axios.get(ACTIONS_ID_TOKEN_REQUEST_URL, {
+      params: {
+        audience: AUDIENCE
+      },
+      headers: {
+        Authorization: `Bearer ${ACTIONS_ID_TOKEN_REQUEST_TOKEN}`
+      }
+    })
+
+    const githubToken = githubTokenRes.value;
+
+    const headers = {
+      Authorization: `Bearer ${githubToken}`
+    }
+
+    const tokenResult = await axios.get(TOKEN_ENDPOINT, {
+      headers: headers
+    })
+
+    const token = tokenResult.value;
+
+    core.setOutput("token", token);
+
+  } catch (error) {
+    console.log(error)
+    core.setFailed(error.message);
+  }
+}
+
+run();
