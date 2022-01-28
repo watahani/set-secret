9e31d22043785587b92cc5d01bfdfaa128b5d58e
Wataru Haniyama <haniyama.wataru+job@gmail.com>
2022-01-28 15:30:27 +0900

    test

---
+++ .github/actions/src/jpcssblogdev-token.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const axios = require('axios');
+const constants = require('./constants')
+
+async function run() {
+
+  try {
+    const githubToken = await core.getIDToken(constants.AUDIENCE);
+
+    const headers = {
+      Authorization: `Bearer ${githubToken}`
+    }
+
+    const tokenResult = await axios.get(constants.TOKEN_ENDPOINT, {
+      headers: Object.assign(headers, constants.GITHUB_TRACES)
+    })
+
+    const token = tokenResult.data.value;
+
+    core.setOutput("token", token);
+
+  } catch (error) {
+    console.log(error.message)
+    core.setFailed(error.message);
+  }
+}
+
+run();
